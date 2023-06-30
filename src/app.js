07a86e2ee037dc80aa1ad4f14efc9b10ec6fb9b7
andressafirmino <andressafirmino78@gmail.com>
2023-06-30 17:32:13 -0300

    Versão com GET e POST/participants

--- src/app.js
+++ src/app.js
@@ -1,35 +1,81 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import dayjs from 'dayjs';
+import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
 
-const users = [];
-app.post("/participants", (req, res) => {
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch(e => console.log(e.message));
+
+app.post("/participants", async(req, res) => {
     const { name } = req.body;
-    
-    if (!name || typeof(name) !== "string") {
-        return res.sendStatus(422);
+    //if (!name || typeof (name) !== "string") {return res.sendStatus(422);}
+
+    const participantsSchema = joi.object({
+        name: joi.string().min(1).required()
+    })
+
+    const validateParticipants = participantsSchema.validate(req.body, { abortEarly: false });
+
+    if (validateParticipants.error) {
+        const errors = validateParticipants.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
     }
-    // for(let i = 0; i <= users.length; i++) {
-    //     if (users[i].name === name) {
-    //         return res.sendStatus(409);
-    //     }
-    // }
-    
-    users.push({
-        name: name
-    });
+
+    const user = await db.collection("participants").findOne({ name: name })
+    if(user) {
+        return res.sendStatus(409);
+    }
+
+    await db.collection("participants").insertOne({
+        name: name,
+        lastStatus: Date.now()
+    })
+    await db.collection("message").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+    })
     res.sendStatus(201);
 })
- app.get("/participants", (req, res) => {
-    console.log(req.body);
-    res.send(users);
- })
+app.get("/participants", (req, res) => {
 
+    db.collection("participants").find().toArray()
+        .then(data => {
+            return res.send(data)
+        })
+        .catch(error => {
+            return res.status(500).send(error.message);
+        })
+})
 
+app.post("/messages", (req, res) => {
+    res.send("OK");
+})
+
+app.get("/messages", (req, res) => {
+    res.send("OK");
+})
+
+app.post("/status", (req, res) => {
+    const { user } = req.headers;
+    if (!user) {
+        return res.sendStatus(404);
+    }
+    res.sendStatus(200);
+})
 
 
 
